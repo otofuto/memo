43fbfc6a5b6765803be6d336e757591d6ca770ae
otofuto <otofuto@gmail.com>
2023-12-13 22:23:53 +0900

    start

---
+++ go.mod
@@ -0,0 +1,11 @@
+module memo
+
+go 1.18
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"memo/pkg/database"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+		if port == "ssl" {
+			port = "443"
+		}
+	}
+	if port == "" {
+		port = "5000"
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/st/", http.StripPrefix("/st/", http.FileServer(http.Dir("./static"))))
+	mux.HandleFunc("/", IndexHandle)
+	log.Println("Listening on port: " + port)
+	if port == "443" {
+		log.Println("SSL")
+		if err := http.Serve(autocert.NewListener(os.Getenv("DOMAIN")), mux); err != nil {
+			panic(err)
+		}
+	} else if err := http.ListenAndServe(":"+port, mux); err != nil {
+		panic(err)
+	}
+}
+
+func IndexHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+
+	if r.Method == http.MethodGet {
+		if r.URL.Path[1:] != "" {
+			w.Header().Add("Content-Disposition", "attachment")
+			tag := r.URL.Path[1:]
+			db := database.Connect()
+			defer db.Close()
+			txt := ""
+			q := "select txt from memo where tag = '" + database.Escape(tag) + "' order by p"
+			rows, err := db.Query(q)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "error 0", 500)
+				return
+			}
+			defer rows.Close()
+			for rows.Next() {
+				var str string
+				err = rows.Scan(&str)
+				if err != nil {
+					log.Println(err)
+					http.Error(w, "error 1", 500)
+					return
+				}
+				txt += str
+			}
+			fmt.Fprint(w, txt)
+			return
+		}
+		http.Error(w, "content nothing", 404)
+		return
+	} else if r.Method == http.MethodPost {
+		if r.FormValue("text") != "" && r.FormValue("tag") != "" {
+			db := database.Connect()
+			defer db.Close()
+			tran, err := db.Begin()
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "error 0", 500)
+				return
+			}
+			tag := strings.TrimSpace(r.FormValue("tag"))
+			txt := r.FormValue("text")
+			page := 0
+			for len([]rune(txt)) > 0 {
+				str := []rune(txt)
+				if len(str) > 10000 {
+					txt = string(str[10000:])
+					str = str[:10000]
+				} else {
+					txt = ""
+				}
+				q := "insert into memo (tag, p, txt) values (?, ?, ?)"
+				ins, err := tran.Prepare(q)
+				if err != nil {
+					log.Println(err)
+					http.Error(w, "error 1", 500)
+					tran.Rollback()
+					return
+				}
+				_, err = ins.Exec(tag, page, string(str))
+				ins.Close()
+				if err != nil {
+					log.Println(err)
+					http.Error(w, "error 2", 500)
+					tran.Rollback()
+					return
+				}
+				page++
+			}
+			tran.Commit()
+			fmt.Fprint(w, "Insert OK")
+			return
+		} else {
+			http.Error(w, "not found", 404)
+		}
+	}
+}
